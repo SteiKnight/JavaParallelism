35aa8ae90d1c215975e374ffa23df52468bc1bdc
delangono <gnxdel001@myuct.ac.za>
2023-08-09 17:07:20 +0200

    added: Constructor

--- ParallelAssignment2023/src/MonteCarloMini/MonteCarloMinimizationParallel.java
+++ ParallelAssignment2023/src/MonteCarloMini/MonteCarloMinimizationParallel.java
@@ -4,19 +4,8 @@ package MonteCarloMini;
 // function. This is the parallel version based on Michelle Kuttel's sequential reference version.
 // @author Delan Gono (steiknight) 2023, UCT.
 
-public class MonteCarloMinimizationParallel implements Runnable{
-  int rows, columns; //grid size
-  
-    	double xmin, xmax, ymin, ymax; //x and y terrain limits
-    	TerrainArea terrain;  //object to store the heights and grid points visited by searches
-    	double searches_density;	// Density - number of Monte Carlo  searches per grid position - usually less than 1!
+public class MonteCarloMinimizationParallel implements Runnable {
 
-  MonteCarloMinimizationParallel(int rows, int columns, double xmin, double xmax, double ymin, double ymax, double searches_density){
-
-  }
-  public void run(){
-
-  }
   static final boolean DEBUG = false;
   static long startTime = 0;
   static long endTime = 0;
@@ -29,4 +18,29 @@ public class MonteCarloMinimizationParallel implements Runnable{
   private static void tock() {
     endTime = System.currentTimeMillis();
   }
+
+  int rows, columns; //grid size
+  double xmin, xmax, ymin, ymax; //x and y terrain limits
+  TerrainArea terrain; //object to store the heights and grid points visited by searches
+  double searches_density; // Density - number of Monte Carlo  searches per grid position - usually less than 1!
+
+  MonteCarloMinimizationParallel(
+    int rows,
+    int columns,
+    double xmin,
+    double xmax,
+    double ymin,
+    double ymax,
+    double searches_density
+  ) {
+    this.rows = rows;
+    this.columns = columns;
+    this.xmin = xmin;
+    this.xmax = xmax;
+    this.ymax = ymax;
+    this.ymin = ymin;
+    this.searches_density = searches_density;
+  }
+
+  public void run() {}
 }
